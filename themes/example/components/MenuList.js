99772bba7a682e90df67324d0d818fbda9b55f46
rouroupuppy <114795565+rouroupuppy@users.noreply.github.com>
2024-06-30 00:24:30 +0800

    Update MenuList.js

--- themes/example/components/MenuList.js
+++ themes/example/components/MenuList.js
@@ -57,7 +57,7 @@ export const MenuList = props => {
   }
 
   return (
-    <nav className='w-full bg-white md:pt-0 px-6 relative z-20 border-t border-b border-gray-light dark:border-hexo-black-gray dark:bg-black'>
+    <nav className='w-full bg-[#41C9E2] md:pt-0 px-6 relative z-20 border-t border-b border-gray-light dark:border-hexo-black-gray dark:bg-black'>
       <div className='mx-auto max-w-4xl md:flex justify-between items-center text-sm md:text-md md:justify-start'>
         <ul className='w-full text-center md:text-left flex flex-wrap justify-center items-stretch md:justify-start md:items-start'>
           {links.map((link, index) => (
